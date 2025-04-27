2953e95ac71390b0b9553fcb44eee3b663db271b
Wassim.Trabelsi <wassim.trabelsi@mssolutions-group.com>
2025-04-27 14:43:09 +0200

    wassim

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! 123");
         System.out.println("Hello iam raniaa");
+        System.out.println("Hello iam wassim");
+        
 
     }
 }
